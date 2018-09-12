35cf4ea15c981117e043ebb47c5704ea376841ca
ProgrammerLeon <leon_huanxiang@gmail.com>
2018-09-12 13:41:09 +0800

    添加其他生成的实体类，example类，mapper和xml

---
+++ src/main/java/com/pgleon/xmall/mapper/ProductImageMapper.java
@@ -0,0 +1,21 @@
+package com.pgleon.xmall.mapper;
+
+import com.pgleon.xmall.pojo.ProductImage;
+import com.pgleon.xmall.pojo.ProductImageExample;
+import java.util.List;
+
+public interface ProductImageMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ProductImage record);
+
+    int insertSelective(ProductImage record);
+
+    List<ProductImage> selectByExample(ProductImageExample example);
+
+    ProductImage selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ProductImage record);
+
+    int updateByPrimaryKey(ProductImage record);
+}
---
+++ src/main/java/com/pgleon/xmall/mapper/ProductMapper.java
@@ -0,0 +1,21 @@
+package com.pgleon.xmall.mapper;
+
+import com.pgleon.xmall.pojo.Product;
+import com.pgleon.xmall.pojo.ProductExample;
+import java.util.List;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    List<Product> selectByExample(ProductExample example);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
