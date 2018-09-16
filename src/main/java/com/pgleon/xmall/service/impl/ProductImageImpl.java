6979597666c8a32c346e2cc42ba444a2c10fac66
ProgrammerLeon <leon_huanxiang@gmail.com>
2018-09-16 23:19:11 +0800

    增加产品图片管理

---
+++ src/main/java/com/pgleon/xmall/service/impl/ProductImageImpl.java
@@ -0,0 +1,49 @@
+package com.pgleon.xmall.service.impl;
+
+import com.pgleon.xmall.mapper.ProductImageMapper;
+import com.pgleon.xmall.pojo.ProductImage;
+import com.pgleon.xmall.pojo.ProductImageExample;
+import com.pgleon.xmall.service.ProductImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductImageImpl implements ProductImageService {
+    @Autowired
+    ProductImageMapper productImageMapper;
+
+    @Override
+    public void add(ProductImage productImage) {
+        productImageMapper.insert(productImage);
+    }
+
+    @Override
+    public void delete(int id) {
+        productImageMapper.deleteByPrimaryKey(id);
+
+    }
+
+    @Override
+    public void update(ProductImage productImage) {
+        productImageMapper.updateByPrimaryKeySelective(productImage);
+    }
+
+    @Override
+    public ProductImage get(int id) {
+
+        return productImageMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List list(int pid, String type) {
+        ProductImageExample example =new ProductImageExample();
+        example.createCriteria()
+                .andPidEqualTo(pid)
+                .andTypeEqualTo(type);
+        example.setOrderByClause("id desc");
+        return productImageMapper.selectByExample(example);
+    }
+
+}
